aa2017cd6b9a18cf0d6cb17882a9365c05ccfdb2
Bramha Vinayak Gulavani <bramhagulavani@gmail.com>
2026-05-28 09:31:13 +0530

    fix - updated text splitter import to langchain-text-splitters package

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 import gradio as gr
